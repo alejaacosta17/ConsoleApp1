582a0888088ca5b4a554ef372a5da2739a6db4db
alejaacosta17 <alejaacosta1707@hotmail.com>
2023-01-13 10:55:49 -0800

    add if and else if

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console; //Console is a static class
 
 namespace ConsoleApp1
 {
@@ -10,7 +11,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            WriteLine("no need to specify the systm.console "); //as static System.Console was specified
+            WriteLine("----------------------------------");
+            WriteLine("*          Name: Alejandra       *");
+            WriteLine("*          Faculty: CBA          *");
+            WriteLine("*          Age: 24               *");
+            WriteLine("----------------------------------");
 
+            WriteLine("How do you feel?");
+            string feeling = Console.ReadLine();
+            WriteLine("I am feeling " +feeling +" Thanks for asking");
+            if (feeling == "good")
+            {
+                WriteLine("I am so happy to hear that");
+            }
+            else if (feeling == "bad")
+            {
+                WriteLine("I am so sorry");
+            }
         }
     }
 }
