977e6614eb3a23d87b0fbe2ab5ee5b7812051886
alejaacosta17 <alejaacosta1707@hotmail.com>
2023-01-13 12:00:07 -0800

    add format pattern

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,29 +11,45 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            WriteLine("no need to specify the systm.console "); //as static System.Console was specified
+            //WriteLine("no need to specify the systm.console "); //as static System.Console was specified
+            //WriteLine("----------------------------------");
+            //WriteLine("*          Name: Alejandra       *");
+            //WriteLine("*          Faculty: CBA          *");
+            //WriteLine("*          Age: 24               *");
+            //WriteLine("----------------------------------");
+
+            //WriteLine("How do you feel?");
+            //string feeling = ReadLine();
+            //WriteLine("I am feeling " +feeling +" Thanks for asking");
+            //if (feeling == "good")
+            //{
+            //    WriteLine("I am so happy to hear that");
+            //}
+            //else if (feeling == "bad")
+            //{
+            //    WriteLine("I am so sorry");
+            //}
+
+            //WriteLine("Demo2: Getting user input");
+            //Write("Enter your name: "); //no tiene salto de linea, el input se hace en la misma linea 
+            //string userName = ReadLine();
+            //Write(userName); //no new line, next command follows here
+
+            Write("Please type your name?");
+            string name = ReadLine();
+            Write("Where are you from");
+            string country = ReadLine();
+
             WriteLine("----------------------------------");
-            WriteLine("*          Name: Alejandra       *");
-            WriteLine("*          Faculty: CBA          *");
-            WriteLine("*          Age: 24               *");
+            WriteLine("*          Name:"+ name +"       *");
+            WriteLine("*       Country:" + country +"   *");
             WriteLine("----------------------------------");
 
-            WriteLine("How do you feel?");
-            string feeling = ReadLine();
-            WriteLine("I am feeling " +feeling +" Thanks for asking");
-            if (feeling == "good")
-            {
-                WriteLine("I am so happy to hear that");
-            }
-            else if (feeling == "bad")
-            {
-                WriteLine("I am so sorry");
-            }
-
-            WriteLine("Demo2: Getting user input");
-            Write("Enter your name: "); //no tiene salto de linea, el input se hace en la misma linea 
-            string userName = ReadLine();
-            Write(userName);
+            WriteLine("Demo 3: Formatted output");
+            string asterik = new string('*', 64);
+            WriteLine(asterik);
+            WriteLine("*{0, 20}: {1, -40}*", "Name", name);
+            WriteLine("*{0, 20}: {1, -40}*", "Country", country);
         }
     }
 }
